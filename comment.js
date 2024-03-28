18779f423a2dd509ca3b6538dfd378562f4e2f78
samim <samimfazlu@gmail.com>
2024-03-28 23:55:04 +0600

    feat(comment): Add comment

---
+++ comment.js
@@ -0,0 +1,12 @@
+//comment CRUD
+// 1. Create a comment
+console.log('Create comment')
+// 2. Read a comment
+
+// console.log('Read comment')
+// // 3. Update a comment
+// console.log('Update comment')
+
+// // 4. Delete a comment
+
+// console.log('Delete comment')
